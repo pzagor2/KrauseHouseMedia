f22da8aea23251457b0a6bfecf1aebb6dbf40978
Greg <rolwesg@gmail.com>
2022-01-23 13:06:02 -0600

    fix: tests

--- src/components/TipButton/TipButton.spec.tsx
+++ src/components/TipButton/TipButton.spec.tsx
@@ -1,10 +1,16 @@
 import { render, screen } from "@testing-library/react";
 
+import ThirdWebProvider from "@/Providers/ThirdWebProvider";
+
 import TipButton from "./TipButton";
 
 describe("TipButton", () => {
   it("should render", () => {
-    render(<TipButton />);
+    render(
+      <ThirdWebProvider>
+        <TipButton />
+      </ThirdWebProvider>
+    );
 
     const button = screen.getByTestId("tip-button");
     expect(button).toBeInTheDocument();
