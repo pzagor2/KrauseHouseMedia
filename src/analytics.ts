db36a8dffbe79fd506caf3a1f3ee7594abbd63ae
gregfromstl <gregfromstl@gmail.com>
2022-02-04 15:52:34 -0600

    fix: only use analytics when deployed

--- src/analytics.ts
+++
@@ -1,6 +0,0 @@
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
-import firebaseConfig from "./firebaseConfig";
-
-initializeApp(firebaseConfig);
-export default getAnalytics();
