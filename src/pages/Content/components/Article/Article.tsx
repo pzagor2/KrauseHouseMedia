77d877f8ef21db49709df864e5393d105620f7a6
gregfromstl <gregfromstl@gmail.com>
2022-02-11 12:31:01 -0600

    feat: add break block type

--- src/pages/Content/components/Article/Article.tsx
+++ src/pages/Content/components/Article/Article.tsx
@@ -1,5 +1,5 @@
 import useArticle from "@/hooks/use-article";
-import Article from "@/types/article";
+import Article, { BlockType } from "@/types/article";
 
 interface ArticleProps {
   id: string;
@@ -13,7 +13,7 @@ export default function Article({ id }: ArticleProps) {
       {article && !error && !isLoading && (
         <div data-testid="content">
           {article?.blocks?.map((block, index) => {
-            if (block.blockType === "paragraph") {
+            if (block.blockType === BlockType.Paragraph) {
               return (
                 <div key={index}>
                   {block.text ?? ""}
@@ -21,20 +21,22 @@ export default function Article({ id }: ArticleProps) {
                   <br />
                 </div>
               );
-            } else if (block.blockType === "video") {
+            } else if (block.blockType === BlockType.Video) {
               return (
                 <div key={index}>
                   <video controls src={block.url} width="100%" height="100%" />
                   <br />
                 </div>
               );
-            } else if (block.blockType === "header") {
+            } else if (block.blockType === BlockType.Header) {
               return (
-                <div key={index} className="text-4xl font-semibold">
+                <div key={index} className="text-4xl font-semibold mb-2">
                   <br />
                   {block.text}
                 </div>
               );
+            } else if (block.blockType == BlockType.Break) {
+              return <hr />;
             }
           })}
         </div>
